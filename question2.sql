48cc36491dc8da6c6b4ee1e53a66ccaea79eadc9
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-09-17 18:43:00 -0300

    Update question2.sql

--- question2.sql
+++ question2.sql
@@ -1,11 +1,11 @@
-Question 2: For this question you’ll need to use SQL. Follow this link to access the data set required for the challenge. Please use queries to answer the following questions. Paste your queries along with your final numerical answers below.
+-- Question 2: For this question you’ll need to use SQL. Follow this link to access the data set required for the challenge. Please use queries to answer the following questions. Paste your queries along with your final numerical answers below.
 
-a) How many orders were shipped by Speedy Express in total?
+-- a) How many orders were shipped by Speedy Express in total?
 SELECT count(*) FROM Orders
 WHERE ShipperID = 1
 
 
-b) What is the last name of the employee with the most orders?
+-- b) What is the last name of the employee with the most orders?
 SELECT LastName FROM Employees
 INNER JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
 GROUP BY LastName
@@ -13,7 +13,7 @@ ORDER BY count(*) DESC
     LIMIT 1;
 
 
-c) What product was ordered the most by customers in Germany?
+-- c) What product was ordered the most by customers in Germany?
 SELECT * FROM Products
 WHERE ProductID IN (SELECT OrderDetails.ProductID FROM Customers
 					INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
